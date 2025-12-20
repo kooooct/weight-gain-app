6c2e17d6434f3dd6e9a4f09453e79506d412e380
kota <koooo13565@gmail.com>
2025-12-20 23:06:25 +0900

    add: weight-gain-app files

---
+++ src/main/java/org/example/weightgainapp/WeightGainAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.weightgainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightGainAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeightGainAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/weightgainapp/WeightGainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.weightgainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightGainAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
